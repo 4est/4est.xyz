1eed1e7e9b10e1f02c7efe9f3f529a3fe1b5c31f
Forrest Peterson <4est90@gmail.com>
2016-08-01 20:09:00 -0400

    gulpfile and gulp-jshint

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+
+//plugins
+
+var jshint = require('gulp-jshint');
+
+//JS hint task
+gulp.task('jshint', function(){
+    gulp.src('./src/scripts/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+    
+})
