7c7318b392db6714e5c3460bd9e18d3d5db6ad3c
Forrest Peterson <forrest.peterson@hpe.com>
2016-08-01 20:21:13 -0400

    imagemin plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,23 @@ var gulp = require('gulp');
 //plugins
 
 var jshint = require('gulp-jshint');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
 
 //JS hint task
 gulp.task('jshint', function(){
     gulp.src('./src/scripts/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+})
+
+//minify images
+gulp.task('imagemin', function(){
+    var imgSrc = './src/images/**/*',
+    imgDst = './build/images';
     
+    gulp.src(imgSrc)
+        .pipe(changed(imgDst))
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgDst));
 })
