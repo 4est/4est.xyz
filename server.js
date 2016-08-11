01260837f6a132e7f7ec67011bc638587e44c34c
Forrest Peterson <forrest.peterson@hpe.com>
2016-08-10 21:42:54 -0400

    fix to dependences and server.js

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+var path = require('path');  
 
-app.use('/src/scripts', express.static(_dirname + '/dist/js');
-app.use('/style', express.static(__dirname + '/dist/css'));
+app.use(express.static(__dirname + '/build'));
+app.use('/src/scripts', express.static(__dirname + '/build/scripts'));
+app.use('/style', express.static(__dirname + '/build/styles'));
 
 app.get("/", function (req, res) {
-  res.render('index');
+  res.sendFile(path.join(__dirname, '/src/index.html'));
+  //res.sendFile('index.html');
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 3500;
 app.listen(port, function() {
   console.log('App listening on port :' + port);
 });
