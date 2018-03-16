918774d8d5a76cdb6680b668b5542c17623bbaa7
Forrest Peterson <forrest.peterson@hpe.com>
2018-03-16 09:54:51 -0400

    js

---
+++ src/main.js
@@ -0,0 +1,20 @@
+
+
+function about() {
+    var cols = document.getElementsByClassName("I");
+    element.classList.remove("hidden");
+}
+
+(function() {
+    var body = document.querySelector('body'),
+    menu = document.querySelector('#menu'),
+    menulink = document.querySelector('.menu-link');
+ 
+     body.classList.add('js');
+
+     menulink.addEventListener('click', function(e){
+     menu.classList.toggle('active');
+     e.preventDefault();
+   });
+
+})();
