79434cba20ad6150748ba96e96dace7b685a7673
Forrest Peterson <forrest.peterson@hpe.com>
2018-03-16 15:47:17 -0400

    fix to scripts

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.use('/src/scripts', express.static(_dirname + '/dist/js');
-app.use('/style', express.static(__dirname + '/dist/css'));
+app.use('/src', express.static(_dirname + '/dist/js'));
+app.use('/', express.static(__dirname + '/dist/css'));
 
 app.get("/", function (req, res) {
   res.render('index');
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.npm_package_config_port;
 app.listen(port, function() {
   console.log('App listening on port :' + port);
-});
-
-var sass = require('node-sass');
-sass.render({
-  file: scss_filename,
-  [, options..]
-}, function(err, result) { /*...*/ });
-// OR
-var result = sass.renderSync({
-  data: scss_content
-  [, options..]
 });
