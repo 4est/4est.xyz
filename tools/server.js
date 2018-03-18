0490ae288b19ff9be928ace33344b9e5187e1b93
Forrest Peterson <forrest.peterson@hpe.com>
2018-03-18 16:52:09 -0400

    needed webpack middleware

--- tools/server.js
+++ tools/server.js
@@ -2,10 +2,17 @@ import express from 'express';
 import webpack from 'webpack';
 import path from 'path';
 import open from 'open';
+import config from '../webpack.config.dev';
 
 
 const port = process.env.npm_package_config_port || 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
