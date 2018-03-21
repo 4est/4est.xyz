1e1455c768c304789602293d4b9d396d2ebbfe32
Forrest Peterson <forrest.peterson@hpe.com>
2018-03-21 16:34:42 -0400

    router

--- src/index.js
+++ src/index.js
@@ -2,21 +2,45 @@ import '../src/style.scss';
 
 
 console.log("ITS WORKING! ITS WORKING!");
-// function about() {
-//     var cols = document.getElementsByClassName("I");
-//     element.classList.remove("hidden");
+
+
+// document.getElementById("I").addEventListener("click", function(){
+//     document.getElementById("header").classList.remove("active-page");
+//     document.getElementById("about").classList.add("active-page");
+// });
+// document.
+// document.getElementById("myBtn").addEventListener("click", function(){
+//     this.innerHTML = "Hello World";
+//     });
+
+
+// function addActive(){
+//     this.classList.remove("active-page");
+//     this.classList.add("active-page");
 // }
 
-// (function() {
-//     var body = document.querySelector('body'),
-//     menu = document.querySelector('#menu'),
-//     menulink = document.querySelector('.menu-link');
- 
-//      body.classList.add('js');
+// elementsArray.forEach(el => el.addEventListener('click', addActive));
 
-//      menulink.addEventListener('click', function(e){
-//      menu.classList.toggle('active');
-//      e.preventDefault();
-//    });
+// let elm = document.getElementById('item');
 
-// })();
+
+
+document.addEventListener('click', Router);
+
+function Router(e){
+    if(e.target.parentElement.className == "menu"){
+        
+        var arr = document.getElementsByClassName("active-page");
+        [].forEach.call(arr, el => el.classList.remove("active-page"));
+
+        if(e.target.id === 'I'){
+            document.getElementById("about").classList.add("active-page");
+        } else if(e.target.id === 'II'){
+            document.getElementById("xp").classList.add("active-page");
+        } else if(e.target.id === 'III'){
+            document.getElementById("projects").classList.add("active-page");
+        } else if(e.target.id === 'IV'){
+            document.getElementById("skills").classList.add("active-page");
+        }
+    }
+  }
