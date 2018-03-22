e8b06e17d1ed12569b049d746c9f9ef674a13cde
Forrest Peterson <forrest.peterson@hpe.com>
2018-03-21 22:32:58 -0400

    hover for tabs

--- src/index.js
+++ src/index.js
@@ -7,28 +7,31 @@ document.addEventListener('click', router);
 
 function router(e){
     if(e.target.parentElement.className == "menu"){
-
         clearActives();
-
         if(e.target.id === 'I'){
+            e.target.classList.add("active-tab");
             document.getElementById("about").classList.add("active-page");
         } else if(e.target.id === 'II'){
+            e.target.classList.add("active-tab");
             document.getElementById("xp").classList.add("active-page");
         } else if(e.target.id === 'III'){
+            e.target.classList.add("active-tab");
             document.getElementById("projects").classList.add("active-page");
         } else if(e.target.id === 'IV'){
+            e.target.classList.add("active-tab");
             document.getElementById("skills").classList.add("active-page");
         }
+    }else if(e.target.className == "home-button"){
+        clearActives();
+        document.getElementById("header").classList.add("active-page");
     }
 }
 
 function clearActives(){
-    var arr = document.getElementsByClassName("active-page");
-    [].forEach.call(arr, el => el.classList.remove("active-page"));
-}
 
-//HOME BUTTON
-document.getElementById("home-button").addEventListener('click',() => {
-    clearActives();
-    document.getElementById("header").classList.add("active-page");
-});
+    var tabs = document.getElementsByClassName("active-tab");
+    [].forEach.call(tabs, el => el.classList.remove("active-tab"));
+
+    var pages = document.getElementsByClassName("active-page");
+    [].forEach.call(pages, el => el.classList.remove("active-page"));
+}
